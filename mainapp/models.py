6d873aa6a174087ec84c1ea92508953b80c43d16
zoonong <zoonongzung@naver.com>
2021-07-04 04:30:01 +0900

    충돌수정

--- mainapp/models.py
+++ mainapp/models.py
@@ -17,5 +17,14 @@ class PostS(models.Model):
     def summary(self):
         return self.body[:20]
 
+    def image_name(self):
+        return self.image.name
+
+class CommentS(models.Model):
+	content = models.TextField()
+	writer = models.ForeignKey(User, on_delete=models.CASCADE)
+	post = models.ForeignKey(PostS, on_delete=models.CASCADE, related_name='comments')
+	created_at = models.DateTimeField(auto_now_add = True)
+	updated_at = models.DateTimeField(auto_now = True)
 
 
