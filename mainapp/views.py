9b84d0b6e42e915bdf80c451124345d3fadd1c6e
donghee <dhk010111@gmail.com>
2021-07-04 03:15:02 +0900

    views.py urls.py media file 수정

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404, redirect
-from .models import PostS
+from .models import PostS, PostD
 from django.utils import timezone
 
 # Create your views here.
@@ -13,7 +13,8 @@ def blogsingle(request):
     return render(request, 'mainapp/blog-single.html')
 
 def blog(request):
-    return render(request, 'mainapp/blog.html')
+    posts = PostD.objects.all()
+    return render(request, 'mainapp/blog.html',{'posts':posts})
 
 def contact(request):
     return render(request, 'mainapp/contact.html')
@@ -31,7 +32,7 @@ def dreamrelay(request):
     return render(request, 'mainapp/dreamrelay.html')
 
 def story(request):
-    posts = PostS.objects.all()
+    posts = PostD.objects.all()
     return render(request, 'mainapp/story.html',{'posts':posts})
 
 def music(request):
@@ -83,3 +84,39 @@ def moana(request):
 
 def Moonight(request):
     return render(request, 'mainapp/Moonight.html')
+
+#Dream_Diary views
+def newD(request):
+    return render(request, 'mainapp/newD.html')
+
+def detailD(request,id):
+    post = get_object_or_404(PostD, id = id)
+    return render(request,'mainapp/detailD.html', {'post':post})
+
+def createD(request):
+    new_post = PostD()
+    new_post.title = request.POST['title']
+    new_post.writer = request.POST['writer']
+    new_post.created_at = timezone.now()
+    new_post.text = request.POST['text']
+    new_post.image = request.FILES['image']
+    new_post.save()
+    return redirect('detailD',new_post.id)
+
+def editD(request,id):
+    edit_post = PostD.objects.get(id = id)
+    return render(request, 'mainapp/editD.html', {'post' : edit_post})
+
+def updateD(request,id):
+    update_post = PostD.objects.get(id = id)
+    update_post.title = request.POST['title']
+    update_post.writer = request.POST['writer']
+    update_post.created_at = timezone.now()
+    update_post.text = request.POST['text']
+    update_post.save()
+    return redirect('detailD',update_post.id)
+
+def deleteD(request, id):
+    delete_post = PostD.objects.get(id = id)
+    delete_post.delete()
+    return redirect('blog')
