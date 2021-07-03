994af6528c819e0dc11b7bfcd7865509ea95633e
donghee <dhk010111@gmail.com>
2021-07-04 03:02:39 +0900

    add PostD at models.py

--- mainapp/models.py
+++ mainapp/models.py
@@ -16,3 +16,11 @@ class PostS(models.Model):
     def summary(self):
         return self.body[:20]
 
+class PostD(models.Model):
+    id = models.AutoField(primary_key = True)
+    title = models.CharField(max_length = 200)
+    writer = models.CharField(max_length = 100)
+    created_at = models.DateTimeField()
+    text = models.TextField()
+    image = models.ImageField(upload_to = 'postD/', blank = True, null = True)
+
