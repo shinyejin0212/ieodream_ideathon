f2f69e8bd5fc473cda64edd9d84bd3f8ee5b6eaa
donghee <dhk010111@gmail.com>
2021-08-12 12:49:38 +0900

    충돌 해결

--- mypage/urls.py
+++ mypage/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
-from . import views
+from django.urls import path ,re_path
+from .import views
+from django.contrib.auth.decorators import login_required
+
 
 app_name="mypage"
-
 urlpatterns=[
-    path('mypage/', views.mypage, name="mypage")
+    path('mypage/', views.mypage, name="mypage"),
+    path('profile_update/', login_required(views.ProfileUpdateView.as_view()), name='profile_update'),
 ]
